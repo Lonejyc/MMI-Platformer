19ccaed58a030b0b441ad3abf53d1bc67e4d6cf1
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-04-24 14:48:58 +0200

    Fixed Jump and Sprite quality

--- Platformer/Assets/Script/PlayerMovement.cs
+++ Platformer/Assets/Script/PlayerMovement.cs
@@ -6,19 +6,26 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 10.0f;
     public bool isJumping = false;
     public bool isGrounded;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
     public Rigidbody2D rb;
     private Vector3 velocity = new Vector3(0, 0, 0);
 
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
         float horizontalMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
-        if(Input.GetButtonDown("Jump"))
+        MovePlayer(horizontalMovement);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
-
-        MovePlayer(horizontalMovement);
     }
 
     void MovePlayer(float _horizontalMovement)
