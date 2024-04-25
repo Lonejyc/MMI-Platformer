678f76a1e4d69c6495df4f3cc294d55b35bcd29d
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-04-26 00:42:40 +0200

    Jump and Movement better, collisions better, animations better

--- Platformer/Assets/Script/PlayerMovement.cs
+++ Platformer/Assets/Script/PlayerMovement.cs
@@ -2,60 +2,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 250.0f;
-    public float jumpForce = 10.0f;
-    public bool isJumping = false;
-    public bool isGrounded;
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
-    public Rigidbody2D rb;
+    public CharacterController2D controller;
+    public float speed = 30.0f;
+    public bool jump = false;
     public Animator animator;
-    public SpriteRenderer spriteRenderer;
-    private Vector3 velocity = new Vector3(0, 0, 0);
+    float horizontalMove = 0f;
 
-    void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
-
-        float horizontalMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
-        MovePlayer(horizontalMovement);
-
-        Flip(rb.velocity.x);
-
-        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-    }
 
     void Update()
     {
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if(Input.GetButtonDown("Jump"))
         {
-            isJumping = true;
+            jump = true;
             animator.SetBool("isJumping", true);
         }
     }
 
-    void MovePlayer(float _horizontalMovement)
+    public void OnLanding()
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
-
-        if(isJumping)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isJumping = false;
-        }
+        animator.SetBool("isJumping", false);
     }
-
-    void Flip(float _velocity)
+    void FixedUpdate()
     {
-        if(_velocity > 0.1f)
-        {
-            spriteRenderer.flipX = false;
-        }
-        else if(_velocity < -0.1f)
-        {
-            spriteRenderer.flipX = true;
-        }
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
     }
 }
